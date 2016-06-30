c3eea11c262127ee1a0759e9d3cc33254b2712dc
Joshua Clayton <joshua.clayton@gmail.com>
2016-06-30 06:20:45 -0400

    Build CLI
    This introduces the initial application, allowing developers to:
    
    * transform values (capitalization, excitedness) via
      * piping into the program
      * reading from a specified file
    
    There are a few issues with the approach, especially as the application
    grows:
    
    * passing Options becomes painful, especially as functionality grows
    * handling failure isn't too troublesome now, but as functionality grows
    * and more areas can trigger failure, it's difficult to understand where
      to handle that, and how best to do so

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,62 @@
 module Main where
 
+import qualified Control.Exception as E
+import qualified Data.Bifunctor as BF
+import qualified Data.Bool as B
+import qualified Data.Char as C
+import           Options.Applicative
+
+-- types
+
+data Options = Options
+    { oCapitalize :: Bool
+    , oExcited :: Bool
+    , oStdIn :: Bool
+    , oFileToRead :: Maybe String
+    }
+
+-- program
+
 main :: IO ()
-main = putStrLn "File Fun"
+main = runProgram =<< parseCLI
+
+runProgram :: Options -> IO ()
+runProgram o =
+    putStr =<< (handleExcitedness o . handleCapitalization o <$> getSource o)
+
+-- data retrieval and transformation
+
+getSource :: Options -> IO String
+getSource o = B.bool (either id id <$> loadContents o) getContents $ oStdIn o
+
+handleCapitalization :: Options -> String -> String
+handleCapitalization o = B.bool id (map C.toUpper) $ oCapitalize o
+
+handleExcitedness :: Options -> String -> String
+handleExcitedness o = B.bool id ("ZOMG " ++) $ oExcited o
+
+loadContents :: Options -> IO (Either String String)
+loadContents o =
+    maybe defaultResponse readFileFromOptions $ oFileToRead o
+  where
+    readFileFromOptions f = BF.first show <$> safeReadFile f
+    defaultResponse = return $ Right "This is fun!"
+
+-- CLI parsing
+
+parseCLI :: IO Options
+parseCLI = execParser (withInfo parseOptions "File Fun")
+  where
+    withInfo opts h = info (helper <*> opts) $ header h
+
+parseOptions :: Parser Options
+parseOptions = Options
+    <$> (switch $ long "capitalize")
+    <*> (switch $ long "excited")
+    <*> (switch $ long "stdin")
+    <*> (optional $ strOption $ long "file")
+
+-- safer reading of files
+
+safeReadFile :: FilePath -> IO (Either E.IOException String)
+safeReadFile = E.try . readFile
